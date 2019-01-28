1edb3518a8d1853411a1069dd320fe2c425a2b70
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 15:17:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ function viewCart() {
 function total() {
   // write your code here
   var total 0;
-  for (let i = 1, l = getcart().length; i < l; i++) {
-
+  for (let i = 1, l = getCart().length; i < l; i++) {
+      total += getCart()[i].itemPrice
   }
 }
 
