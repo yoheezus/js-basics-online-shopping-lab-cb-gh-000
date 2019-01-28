701a3459f2b6c783d78cd1e02eea4a76e62487e2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 15:47:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function placeOrder(cardNumber) {
   if (cardNumber) {
       return `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`
   }
-    setCart([])
+  setCart([])
   return "Sorry, we don't have a credit card on file for you."
 }
 
