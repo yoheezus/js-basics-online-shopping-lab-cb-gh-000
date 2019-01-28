ececd9c50e98a43f64aac616a071f221ece5dc40
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 14:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
   } else if (cart.length === 2) {
       return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}, and ${cart[1].itemName} at \$${cart[1].itemPrice}.`
   } else {
-      return "empty"
+      return formatListOfThree()
   }
 }
 
