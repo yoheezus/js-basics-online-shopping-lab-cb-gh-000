865b114250fca5e81bae204511c8e6ba266f9f9d
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 14:17:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (!cart.length) {
+      return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+      let oneItem = cart[0]
+      return `In your cart, you have ${oneItem.itemName} at \$${oneItem.itemPrice}`
+  }
 }
 
 function total() {
