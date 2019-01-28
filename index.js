470c186e508b4862c78c0552320fa94ef59e08dd
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 15:48:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function viewCart() {
   if (!cart.length) {
       return "Your shopping cart is empty."
   } else if (cart.length === 1) {
-      let oneItem = cart[0]
-      return `In your cart, you have ${oneItem.itemName} at \$${oneItem.itemPrice}.`
+      let oneItem = cart[0];
+      return `In your cart, you have ${oneItem.itemName} at \$${oneItem.itemPrice}.`;
   } else if (cart.length === 2) {
-      return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}, and ${cart[1].itemName} at \$${cart[1].itemPrice}.`
+      return `In your cart, you have ${cart[0].itemName} at \$${cart[0].itemPrice}, and ${cart[1].itemName} at \$${cart[1].itemPrice}.`;
   } else {
       return formatListOfThree();
   }
