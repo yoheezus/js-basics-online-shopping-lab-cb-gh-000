93f9c0f0297cdd08c7b40ed79ec001b2099e4e6a
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 15:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function removeFromCart(item) {
   // write your code here
   var editableCart = getCart()
   for (let i = 0, l = getCart().length; i < l; i++) {
-      if (item === getCart()[i].itemName) {
+      if (item === editableCart[i].itemName) {
           editableCart = getCart().splice(i);
           return editableCart
       }
